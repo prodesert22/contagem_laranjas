d5fc078e9f39cac9d6b5841c63bac366b4370e3d
Pedro <pedroantonioppms@gmail.com>
2024-11-11 12:38:12 -0400

    arrumado a execução do detecção_cv2

--- deteccao_cv2.py
+++ deteccao_cv2.py
@@ -2,22 +2,22 @@ import cv2
 import numpy as np
 from cv2.typing import MatLike
 
-caminho_imagem = "image.jpg"
+caminho_imagem = "laranja.jpg"
 
 def pre_processamento(caminho_imagem: str):
     # Carrega a imagem
     imagem = cv2.imread(caminho_imagem)
-    cv2.imshow("imagem", imagem)
+    #cv2.imshow("imagem", imagem)
     # Converte a imagem para HSV
     hsv = cv2.cvtColor(imagem, cv2.COLOR_BGR2HSV)
-    cv2.imshow("HSV", hsv)
+    #cv2.imshow("HSV", hsv)
     # Define os limites inferior e superior para a cor laranja
     limite_inferior = np.array([10, 100, 20], dtype="uint8")
     limite_superior = np.array([25, 255, 255], dtype="uint8")
 
     # Cria uma máscara para a cor laranja
     mascara = cv2.inRange(hsv, limite_inferior, limite_superior)
-    cv2.imshow("mascara", mascara)
+    #cv2.imshow("mascara", mascara)
 
     # Define a cor preta e branca
     preto = (0, 0, 0)
@@ -25,10 +25,7 @@ def pre_processamento(caminho_imagem: str):
 
     # Pinta de preto as áreas fora da máscara da laranja
     imagem[mascara == 0] = preto
-    cv2.imshow("imagem mascara_invertida", imagem)
-    # Pinta de branco as áreas da máscara da laranja
-    #imagem[mascara == 255] = branco
-    #cv2.imshow("mascara", imagem)
+    #cv2.imshow("imagem mascara_invertida", imagem)
 
     # Encontra os contornos da máscara da laranja
     # É criado os contornos para preencher as falhas no inteiror da laranja
@@ -48,21 +45,18 @@ def pre_processamento(caminho_imagem: str):
             # Pinta de branco os pixels dentro do contorno
             imagem[mascara_contorno == 255] = branco
 
-    cv2.imshow("contorno", imagem)
+    #cv2.imshow("contorno", imagem)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
 
-    filtrada = cv2.dilate(imagem, kernel, iterations = 3)
-    filtrada = cv2.erode(filtrada, kernel,  iterations = 6)
-    filtrada = cv2.dilate(filtrada, kernel, iterations = 4)
-    filtrada = cv2.erode(filtrada, kernel,  iterations = 6)
+    img_processada = cv2.dilate(imagem, kernel, iterations = 3)
+    img_processada = cv2.erode(img_processada, kernel,  iterations = 6)
+    img_processada = cv2.dilate(img_processada, kernel, iterations = 4)
+    img_processada = cv2.erode(img_processada, kernel,  iterations = 6)
 
-    #cv2.imshow("filtrada", filtrada)
-    #cv2.waitKey(0)
-
-    filtrada = cv2.cvtColor(filtrada, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("filtrada", filtrada)
-    return filtrada
+    img_processada = cv2.cvtColor(img_processada, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("img processada", img_processada)
+    return img_processada
 
 def contar_objetos_opencv(imagem: MatLike, caminho_imagem: str):
     # Encontrar componentes conectados
@@ -90,7 +84,7 @@ def contar_objetos_opencv(imagem: MatLike, caminho_imagem: str):
         # Filtra objetos com a area maior que 100 pixeis
         if area >= 100 and w * h < height * width:
             # Desenha a caixa de detecção
-            cv2.rectangle(img_com_caixa, (x, y), (x + w, y + h), (0, 255, 0), 3)
+            cv2.rectangle(img_com_caixa, (x, y), (x + w, y + h), (0, 255, 0), 2)
             num_obj += 1
 
     return num_obj, img_com_caixa
@@ -118,16 +112,17 @@ def contar_objetos_skimage(imagem: MatLike, caminho_imagem: str):
     
     return num_obj, img_com_caixa
 
-img_processada = pre_processamento(caminho_imagem)
+if __name__ == "__main__":
+    img_processada = pre_processamento(caminho_imagem)
 
-num_obj_cv2, img_cv2 = contar_objetos_opencv(img_processada, caminho_imagem)
+    num_obj_cv2, img_cv2 = contar_objetos_opencv(img_processada, caminho_imagem)
 
-num_obj_skimage, img_skimage = contar_objetos_skimage(img_processada, caminho_imagem)
+    num_obj_skimage, img_skimage = contar_objetos_skimage(img_processada, caminho_imagem)
 
-# Exibir a imagem com os retângulos
-print(f"cv2 - total de lanrajas: {num_obj_cv2}")
-cv2.imshow("Imagem com Retangulos cv2", img_cv2)
-print(f"scikit-image - total de lanrajas: {num_obj_skimage}")
-cv2.imshow("Imagem com Retangulos scikit-image", img_skimage)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Exibir a imagem com os retângulos
+    print(f"cv2 - total de lanrajas: {num_obj_cv2}")
+    cv2.imshow("Imagem com Retangulos cv2", img_cv2)
+    print(f"scikit-image - total de lanrajas: {num_obj_skimage}")
+    cv2.imshow("Imagem com Retangulos scikit-image", img_skimage)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
