f511b2d3240205a9ecb8412d0e1538eb96551b34
Pedro <pedroantonioppms@gmail.com>
2024-11-11 14:24:56 -0400

    adicionada functiona para conter objetos com yolo

--- deteccao.py
+++ deteccao.py
@@ -1,29 +1,30 @@
 from ultralytics import YOLO
 
-# Carrega o modelo
-model = YOLO("./treinamento/yolov5/weights/best.pt")
+def contar_objetos(modelo: str, imagem: str, conf=0.5):
+    # Carrega o modelo
+    model = YOLO(modelo)
 
-# Faz as predições
-results = model.predict(source="./test.jpg", conf=0.2)
+    # Faz as predições
+    results = model.predict(source=imagem, conf=conf)
 
-# Pega as caixas detectadas e o nome das classes qie tem no modelo
-boxes = results[0].boxes
-names = results[0].names
+    # Pega as caixas detectadas e o nome das classes qie tem no modelo
+    boxes = results[0].boxes
+    names = results[0].names
 
-if boxes:
-    # Dicionario para contar quantos objetos de uma classe foram detectados
     count = {}
-    for key in names.keys():
-        count[key] = 0
-    
-    # Conta os objetos de cada classe e adiciona no dicionario count
-    classes_objetos = boxes.cls.cpu().numpy()
-    for classe in classes_objetos:
-        classe = int(classe)
-        count[classe] = count[classe] + 1
+    if boxes:
+        # Dicionario para contar quantos objetos de uma classe foram detectados
+        for key in names.keys():
+            count[key] = 0
+        # Conta os objetos de cada classe e adiciona no dicionario count
+        classes_objetos = boxes.cls.cpu().numpy()
+        for classe in classes_objetos:
+            classe = int(classe)
+            count[classe] = count[classe] + 1
 
-    for key, value in count.items():
-        print(f"Total detectado: {value} {names[key]}")
+    # Mostra a imagem com as caixas de detecção
+    results[0].save("resultado_" + imagem)
+    return count, "resultado_" + imagem
 
-# Mostra a imagem com as caixas de detecção
-results[0].show()
+if __name__ == "__main__":
+    contar_objetos("./treinamento/yolov5/weights/best.pt", "laranjas.jpg")
