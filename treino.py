59f7e1347d2470c3e1c949c22a96230727899c80
Pedro <pedroantonioppms@gmail.com>
2024-11-14 13:00:47 -0400

    corrigido o nome dos modelos e aumentado o numero de epocas para 100

--- treino.py
+++ treino.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO, settings
 
 def main():
     # Carrega o modelo pretreinado do YOLO
-    # YOLO 5 - yolov5n.pt
+    # YOLO 5 - yolov5nu.pt
     # YOLO 8 - yolov8n.pt
     # YOLO 9 - yolov9c.pt
     # YOLO 11 - yolo11n.pt
@@ -12,7 +12,7 @@ def main():
     settings.update({"datasets_dir": os.path.join(os.path.abspath("."), "dataset")})
 
     # Treina o modelo usando o dataset de laranjas
-    model.train(data="./data.yaml", epochs=50)
+    model.train(data="./data.yaml", epochs=100)
 
 
 if __name__ == "__main__":
